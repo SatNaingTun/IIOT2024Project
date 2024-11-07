e624bc1ba2a46f61bd48c331b0ddc6b044b32cd2
Sat Naing Tun <satnaingtun.snt@gmail.com>
2024-11-07 15:42:49 +0700

    modbus test 2

--- IIOTWeb/iiot/Controllers/ModbusPLC.py
+++ IIOTWeb/iiot/Controllers/ModbusPLC.py
@@ -33,30 +33,33 @@ def read_coil(client, address, quantity):
     else:
         print(f"Failed to read coils from address {address}")
 
-def read_reg(address,quantity):
+def read_reg(address,count=1,slave=1):
     # result=client.read_holding_registers(address=address,count=quantity,slave=1)
     # read=client.read_holding_registers(address = 31249 ,count =2,unit=1)
     # read.registers
 
-    result = client.read_holding_registers(address=530, count=1, slave=1)
-
+    # address=address-40001
+    result = client.read_holding_registers(address=address, count=count,slave=slave)
+    # result = client.read_holding_registers(address=530, count=1,slave=1)
+    
     # result = client.read_holding_registers(register,count=2,unit=1).registers
     # decoder = BinaryPayloadDecoder.fromRegisters(result,byteorder= Endian.Big, wordorder=Endian.Little)
     # value = decoder.decode_32bit_float()
     # print(value)
-
+    print(result.registers)
     if not result.isError():
-        print("Successfully read function code: %s" % result.function_code)
+        # print("Successfully read function code: %s" % result.function_code)
         print("Result:",result.registers)
+        return result.registers
     else:
-        print(f"Failed to read function code from address {address}")
+        logger.error(f"Error reading register from address {address}")
 
-client = ModbusTcpClient(host=tcp_host, port=tcp_port)
+# client = ModbusTcpClient(host=tcp_host, port=tcp_port)
 
 if __name__=='__main__':
     try:
         # while True:
-            client = ModbusTcpClient(tcp_host,port=tcp_port)
+            client = ModbusTcpClient(host='192.168.200.2',port=504)
             connected=client.connect()
             logger.info(connected)
             if connected:
@@ -64,12 +67,13 @@ if __name__=='__main__':
                 # value_to_write = not value_to_write
 
                 # read_coil(client, coil_address, num_to_read)
-                read_reg(address=530,quantity=1)
+                read_reg(address=530,count=1,slave=1)
 
-                client.close()
+                # client.close()
             else:
                 print(f"Failed to connect to Modbus TCP on {tcp_host}")
+                client.close()
 
             time.sleep(1)
     except KeyboardInterrupt:
-        pass
+        print(KeyboardInterrupt)
