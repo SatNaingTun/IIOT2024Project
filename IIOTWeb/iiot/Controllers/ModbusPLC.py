60a0eb18bc6beaa12afab5d8fa880a7b5363da48
Sat Naing Tun <satnaingtun.snt@gmail.com>
2024-11-08 22:59:13 +0700

    modbus read data for all addresses

--- IIOTWeb/iiot/Controllers/ModbusPLC.py
+++ IIOTWeb/iiot/Controllers/ModbusPLC.py
@@ -24,7 +24,7 @@ def connectConnection(host='192.168.200.2',port=102):
     connected=client.connect()
     return connected
 
-def write_coil(client, address, value):
+def write_coil(address, value):
     result = client.write_coil(address, value, 1)
 
     if not result.isError():
@@ -32,35 +32,47 @@ def write_coil(client, address, value):
     else:
         print(f"Failed to write value to coil address {address}")
 
-def read_coil(client, address, quantity):
-    result = client.read_coils(address, quantity)
+def read_coil(address, count=1,slave=1):
+    result = client.read_coils(address, count)
 
     if not result.isError():
         print("Successfully read coils: %s" % result.bits)
+        return result.registers[0]
     else:
         print(f"Failed to read coils from address {address}")
 
 def read_reg(address,count=1,slave=1):
-    # result=client.read_holding_registers(address=address,count=quantity,slave=1)
-    # read=client.read_holding_registers(address = 31249 ,count =2,unit=1)
-    # read.registers
-
-    # address=address-40001
     result = client.read_holding_registers(address=address, count=count,slave=slave)
-    # result = client.read_holding_registers(address=530, count=1,slave=1)
-    
-    # result = client.read_holding_registers(register,count=2,unit=1).registers
-    # decoder = BinaryPayloadDecoder.fromRegisters(result,byteorder= Endian.Big, wordorder=Endian.Little)
-    # value = decoder.decode_32bit_float()
-    # print(value)
     print(result.registers)
     if not result.isError():
-        # print("Successfully read function code: %s" % result.function_code)
         print("Result:",result.registers)
-        return result.registers
+        return result.registers[0]
     else:
         logger.error(f"Error reading register from address {address}")
         return None
+    
+def readData(address,count=1,slave=1):
+    if(0<address<10000):
+        address=address-10000
+        result=client.read_coils(address, count)
+    elif(10000<address<20000):
+        address=address-10000
+        result=client.read_discrete_inputs(address=address, count=count,slave=slave)
+    elif(30000<address<40000):
+        address=address-10000
+        result=client.read_input_registers(address=address, count=count,slave=slave)
+    elif(40000<address<50000):
+        address=address-10000
+        result=client.read_holding_registers(address=address, count=count,slave=slave)
+    
+    if not result.isError():
+        print("Result:",result.registers)
+        return result.registers[0]
+    else:
+        logger.error(f"Error reading register from address {address}")
+        return None
+    
+                 
 
 # client = ModbusTcpClient(host=tcp_host, port=tcp_port)
 
@@ -76,7 +88,7 @@ if __name__=='__main__':
                 # value_to_write = not value_to_write
 
                 # read_coil(client, coil_address, num_to_read)
-                read_reg(address=531,count=1,slave=1)
+                readData(address=531,count=1,slave=1)
 
                 client.close()
             else:
