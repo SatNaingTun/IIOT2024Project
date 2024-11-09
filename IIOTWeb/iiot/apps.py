0a3174119d903bf0f5cf127ad900a167bc7c26d1
Sat Naing Tun <satnaingtun.snt@gmail.com>
2024-11-09 11:44:29 +0700

    test background running

--- IIOTWeb/iiot/apps.py
+++ IIOTWeb/iiot/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class IiotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'iiot'
+
+    # def ready(self):
+    #     from .DataCollector import getCollectBySchedule
+    #     getCollectBySchedule(1)
+        # return super().ready()
