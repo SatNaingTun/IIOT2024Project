4c53501a5b9cbd9a3d637f84be564551a14537d8
Sat Naing Tun <satnaingtun.snt@gmail.com>
2025-04-01 12:30:15 +0700

    merged 01-04-2025

--- IIOTWeb/iiot/apps.py
+++ IIOTWeb/iiot/apps.py
@@ -6,8 +6,8 @@ class IiotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'iiot'
 
-    def ready(self):
-        from .DataCollector import getCollectBySchedule
-        rt=getCollectBySchedule(2)
+    # def ready(self):
+    #     from .DataCollector import getCollectBySchedule
+    #     rt=getCollectBySchedule(2)
 
        
